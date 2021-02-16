337dbbfe4e5193997f6ef14ba2f4fc4bb21a8255
Veronica Palafox <veronicapalafox@Veronicas-iMac.attlocal.net>
2021-02-15 22:37:02 -0800

    dailycodechallenge

---
+++ dailycodechallenge04.js
@@ -0,0 +1,12 @@
+//Convert a String to a Number!
+var stringToNumber = function(str){
+    // put your code here
+    return parseInt(str)
+  }
+
+//Convert a Number to a String!
+
+function numberToString(num) {
+    // Return a string of the number here!
+    return num.toString()
+  }
