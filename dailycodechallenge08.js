6c767c4c77f8e354f789804f7f7f5f37e967e7c3
Veronica Palafox <veronicapalafox@Veronicas-iMac.attlocal.net>
2021-02-20 00:46:59 -0800

    dailycodechallenge

---
+++ dailycodechallenge08.js
@@ -0,0 +1,14 @@
+//square every digit 
+
+
+function squareDigits(num){
+  
+  const digits = num.toString().split('')
+  console.log(digits) 
+  const newDigits = digits.map(x => x * x)
+  console.log(newDigits)
+  const newNewDigits = newDigits.join('')
+  console.log(newNewDigits)
+    return +newNewDigits
+    
+  }
