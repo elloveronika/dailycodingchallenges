5de197d488683e789c62394ce0545b0b69309eac
Veronica Palafox <veronicapalafox@Veronicas-iMac.attlocal.net>
2021-02-22 00:25:02 -0800

    binary string

---
+++ dailycodechallenge10.js
@@ -0,0 +1,16 @@
+//Given a string of digits, you should replace any digit below 5 with '0' and any digit 5 and above with '1'. Return the resulting string.
+
+function fakeBin(arr){
+  
+    let storage = ''
+    
+     
+   for(let i = 0 ; i < arr.length ; i++){
+   if(arr[i] < 5){
+      storage += 0//try to find a way to add on to itself
+   }else if(arr[i] >= 5){
+     storage += 1
+   }
+    
+   } return storage
+     }
