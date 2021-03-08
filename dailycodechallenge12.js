e302164456f6001800b17b7f997f7d6c7f160d9c
elloveronika <ello.veronika92@gmail.com>
2021-03-08 01:26:35 -0800

    fundementals

---
+++ dailycodechallenge12.js
@@ -0,0 +1,12 @@
+function feast(beast, dish) {
+  
+      if(beast.charAt(0) == dish.charAt(0) && beast.charAt(beast.length-1) == dish.charAt(dish.length-1) ){
+        return true
+      }else{
+        return false
+      }
+      
+      
+      
+      
+    }
---
+++ dailycodingchallenge13.js
@@ -0,0 +1,5 @@
+function move (position, roll) {
+    // return the new position
+    let newSpot = position + (roll*2)
+    return newSpot
+  }
