16ff8a5e5111f7765231cfb655f67270873b7676
elloveronika <ello.veronika92@gmail.com>
2021-03-16 03:54:28 -0700

    math conversion

---
+++ dailycodingchallenge19.js
@@ -0,0 +1,9 @@
+//find a fucntion that willl convert time into liters if 1 hour is eequal to 0.5 liters of water to stay hydrated and healthy
+
+function litres(time) {
+  
+  
+    let answer = (time * .5)
+    
+    return Math.floor(answer);
+  }
