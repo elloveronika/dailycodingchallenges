22e29c1ab8f8b080f18e8c9713c152002d9f5b6a
elloveronika <ello.veronika92@gmail.com>
2021-03-23 20:41:20 -0700

    array method push

---
+++ dailycodingchallenge22.js
@@ -0,0 +1,13 @@
+function between(a, b) {
+
+    let myArray= []
+    
+   for (let i = a ; i <= b ; i++){
+      myArray.push(i)
+   }
+  return myArray
+  
+  }
+
+
+  //push all the numbers in between a and b into an array
