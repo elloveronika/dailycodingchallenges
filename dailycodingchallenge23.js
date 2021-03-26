02ca028f59431fa643a9fd7b170f8392c0c2dc08
elloveronika <ello.veronika92@gmail.com>
2021-03-26 11:35:36 -0700

    iterate through multiples

---
+++ dailycodingchallenge23.js
@@ -0,0 +1,12 @@
+function countBy(x, n) {
+    var z = [];
+    
+      for (let i = 1 ; i <= n ; i++){
+        z.push(i * x)
+      }
+        
+  
+    return z;
+  }
+
+  //multiply each number in the array by same number in the amount of times as n
