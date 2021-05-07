0811534a30b5603c8e5a01f43cd8a443773d5f17
elloveronika <ello.veronika92@gmail.com>
2021-05-06 23:56:46 -0700

    trianglularAngles

---
+++ dailycoding4.js
@@ -0,0 +1,12 @@
+function mango(quantity, price){
+    return price * (quantity - (Math.floor(quantity / 3 ) ))
+     
+      
+    }
+    
+    //p: quanitity of mangoes and the price for each mogoo as nums
+    //r: the total price including the free mogoo
+    //e: 3 mogoos for the of 2
+    //p: i have 3 mogoos, they are 3 a pop, 
+    // 300 / 3 
+    // 300 - 300/3
