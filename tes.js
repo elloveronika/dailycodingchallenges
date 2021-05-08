14ecac8117bb573533d4c1a6eec6a8441f05eff6
elloveronika <ello.veronika92@gmail.com>
2021-05-08 01:07:49 -0700

    falsy

---
+++ tes.js
@@ -0,0 +1,16 @@
+function sumStr(a,b) {
+
+    if(a == "") {
+      return 0
+    }else if (b == ""){
+      return 0
+      }else{
+    let  yo= parseInt(a)
+    let sup= parseInt(b)
+    let newS = yo+sup
+      return newS.toString()
+    }
+    
+  
+    
+  }
